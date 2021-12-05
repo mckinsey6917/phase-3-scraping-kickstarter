07d314809e01f4b4db29d01985f9d7fb806f42b1
mckinseyleckenby <leckenbym11@gmail.com>
2021-12-05 14:20:54 -0600

    commit tree 06238907dd6b59e60622572db7eefbf27cae69f9

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,32 @@
 # require libraries/modules here
+require 'nokogiri'
+require 'pry'
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard-blurb").text
+# location: project.css("ul.project-meta").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
 def create_project_hash
   # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+
+  #now set up empty projects hash that we fill up with scraped data:
+  projects = {}
+  #then Iterate through the projects w loop
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard-blurb").text,
+      :location => project.css("ul.project-meta").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i,
+
+    }
+  end
+
+  #thenthen return the projects hash taht we filled above
+  projects
 end
